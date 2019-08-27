d2f703e411dae981ee23d8a8bc9391520d1b435b
e-peters-waternet <edwin.peters@waternet.nl>
2019-08-27 13:18:49 +0200

    changed setup.py to use the forked repo

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
 import setuptools
 
+# setuptools.setup(
+#     name="robotframework-excellentlibrary",
+#     version="0.9.3",
+#     author="Bart Kleijngeld",
+#     author_email="bartkl@gmail.com",
+#     description="A really useful Robot Framework library for working with Excel 2010 (and above) files.",
+#     url="https://github.com/bartkl/robotframework-excellentlibrary",
+#     packages=setuptools.find_packages(),
+#     classifiers=(
+#         "Programming Language :: Python :: 2",
+#         "Operating System :: OS Independent",
+#     ),
+#     python_requires="~=2.7",
+#     install_requires="openpyxl>=2.5.4",
+#     keywords="excel testing robotframework robotframework-excellibrary robotframework-excellib"
+# )
+
 setuptools.setup(
     name="robotframework-excellentlibrary",
     version="0.9.3",
     author="Bart Kleijngeld",
     author_email="bartkl@gmail.com",
-    description="A really useful Robot Framework library for working with Excel 2010 (and above) files.",
-    url="https://github.com/bartkl/robotframework-excellentlibrary",
+    description="A really useful Robot Framework library for working with Excel 2010 (and above) files and Python 3.",
+    url="https://github.com/e-peters-waternet/robotframework-excellentlibrary",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
-    python_requires="~=2.7",
-    install_requires="openpyxl>=2.5.4",
+    python_requires="~=3.0",
+    install_requires="openpyxl>=3.0.0",
     keywords="excel testing robotframework robotframework-excellibrary robotframework-excellib"
 )
